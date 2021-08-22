c1a5bd7e353cee57fd10795649d3ef2a16aca012
hyksm <hiyoko3m.tech@gmail.com>
2021-08-23 00:48:30 +0900

    update(backend): add tests and refine the schema

--- gcp/page_sync_backend/app/main.py
+++ gcp/page_sync_backend/app/main.py
@@ -1,7 +1,8 @@
 from typing import Optional
 from uuid import uuid4
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, Path, status, WebSocket, WebSocketDisconnect
+from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -9,6 +10,10 @@ app = FastAPI()
 
 class RoomID(BaseModel):
     room_id: str
+
+
+class SlideOperation(BaseModel):
+    direction: str
 
 
 class RoomManager:
@@ -32,10 +37,13 @@ class RoomManager:
         if websocket is not None:
             await websocket.close()
 
-    async def send_message(self, room_id: str, message: str):
+    async def operate_slide(self, room_id: str, direction: str) -> bool:
         websocket = self.connections.get(room_id, None)
         if websocket is not None:
-            await websocket.send_text(message)
+            await websocket.send_json(SlideOperation(direction=direction).dict())
+            return True
+        else:
+            return False
 
 
 manager = RoomManager()
@@ -53,8 +61,8 @@ async def create_room():
     return RoomID(room_id=room_id)
 
 
-@app.websocket("/room-ws")
-async def open_connection(websocket: WebSocket, room_id: str):
+@app.websocket("/room-ws/{room_id}")
+async def open_connection(websocket: WebSocket, room_id: str = Path(...)):
     if not await manager.connect(room_id, websocket):
         return
 
@@ -66,19 +74,21 @@ async def open_connection(websocket: WebSocket, room_id: str):
 
 
 @app.post(
-    "/room/prev",
+    "/room/{room_id}/prev",
     summary="back to the previous slide",
     description="Go to the previous slide of the host of the room.",
 )
-async def back_to_prev_slide(room_id: str):
-    await manager.send_message(room_id, "prev")
+async def back_to_prev_slide(room_id: str = Path(...)):
+    if not await manager.operate_slide(room_id, "prev"):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
 
 @app.post(
-    "/room/next",
+    "/room/{room_id}/next",
     summary="go to the next slide",
     description="Go to the next slide of the host of the room.",
     response_model=None,
 )
-async def go_to_next_slide(room_id: str):
-    await manager.send_message(room_id, "next")
+async def go_to_next_slide(room_id: str = Path(...)):
+    if not await manager.operate_slide(room_id, "next"):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
